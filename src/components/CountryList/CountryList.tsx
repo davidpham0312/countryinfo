4a4cd9a5e3d8871ab2713e183e9ebf678bc77fd2
davidpham <77518288+davidpham0312@users.noreply.github.com>
2023-02-08 10:47:42 +0200

    feat: add table header and search function

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { CountryListProps } from "../../types";
 import styles from "./countryList.module.css";
 
+function isCharsIncluded(searchString: string, targetString: string) {
+  return searchString
+    .toLocaleLowerCase()
+    .split("")
+    .every((char) => targetString.toLocaleLowerCase().includes(char));
+}
+
 export const CountryList: React.FC<CountryListProps> = ({ listCollection }) => {
   const countryList = listCollection ?? [];
+  const [search, setSearch] = useState("");
+  const handleChange = (e: { target: { value: string } }) => {
+    setSearch(e.target.value);
+  };
   return (
-    <ul>
-      {countryList.map(({ name, flag, population, region, flags}) => (
-        <li key={flag} className = {styles.country_list__single_country}>
-          <img src={flags.png} alt={flags.alt? flags.alt : ""} className={styles.country_list__flag}/>
-          <b className={styles.country_list__name}>{name.common}</b>
-          <div className={styles.country_list__region}>{region}</div>
-          <div className={styles.country_list__population}>{population}</div>
-        </li>
-      ))}
+    <ul className={styles.country_list__container}>
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Search by country name..."
+        className={styles.country_list__search}
+      />
+      <li className={styles.country_list__title}>
+        <div className={styles.country_list__flag}>Flag</div>
+        <div className={styles.country_list__name}>Name</div>
+        <div className={styles.country_list__region}>Region</div>
+        <div className={styles.country_list__population}>Population</div>
+      </li>
+      {countryList.map(({ name, flag, population, region, flags }) => {
+        if (isCharsIncluded(search, name.common)) {
+          return (
+            <li key={flag} className={styles.country_list__single_country}>
+              <img
+                src={flags.png}
+                alt={flags.alt ? flags.alt : ""}
+                className={styles.country_list__flag}
+              />
+              <b className={styles.country_list__name}>{name.common}</b>
+              <div className={styles.country_list__region}>{region}</div>
+              <div className={styles.country_list__population}>
+                {population}
+              </div>
+            </li>
+          );
+        }
+        return null;
+      })}
     </ul>
   );
 };
---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export interface CountryProps {
+  name: CountryNameProps;
+  flag: string;
+  population: number;
+  region: string;
+  languages: CountryLanguageProps;
+  flags: CountryFlagProps;
+}
+export interface CountryListProps {
+  listCollection: CountryProps[];
+}
+
+export interface CountryNameProps {
+  common: string;
+  official: string;
+}
+
+export type CountryLanguageProps= {
+  [key: string]: string;
+}
+export interface CountryFlagProps {
+  png: string,
+  svg: string,
+  alt: string,
+}
