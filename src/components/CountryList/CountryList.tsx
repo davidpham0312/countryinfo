dce91fb3f540adc4f0696f4c243f007e0571d4c7
davidpham <77518288+davidpham0312@users.noreply.github.com>
2023-02-08 12:15:09 +0200

    feat: sorting name and population

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -12,6 +12,40 @@ function isCharsIncluded(searchString: string, targetString: string) {
 export const CountryList: React.FC<CountryListProps> = ({ listCollection }) => {
   const countryList = listCollection ?? [];
   const [search, setSearch] = useState("");
+  const [countryAscending, setCountryAscending] = useState(1);
+  const [populationAscending, setPopulationAscending] = useState(0);
+
+  function nameButtonClicked() {
+    setPopulationAscending(0);
+    countryAscending === 0 || countryAscending === -1
+      ? setCountryAscending(1)
+      : setCountryAscending(-1);
+  }
+
+  function populationButtonClicked() {
+    setCountryAscending(0);
+    populationAscending === 0 || populationAscending === -1
+      ? setPopulationAscending(1)
+      : setPopulationAscending(-1);
+  }
+
+  if (countryAscending === 1)
+    countryList.sort((a, b) =>
+      a.name.common > b.name.common ? 1 : b.name.common > a.name.common ? -1 : 0
+    );
+  else if (countryAscending === -1)
+    countryList.sort((a, b) =>
+      b.name.common > a.name.common ? 1 : a.name.common > b.name.common ? -1 : 0
+    );
+
+    if (populationAscending === 1)
+    countryList.sort((a, b) =>
+      a.population > b.population ? 1 : b.population > a.population ? -1 : 0
+    );
+  else if (populationAscending === -1)
+    countryList.sort((a, b) =>
+      b.population > a.population ? 1 : a.population > b.population ? -1 : 0
+    );
   const handleChange = (e: { target: { value: string } }) => {
     setSearch(e.target.value);
   };
@@ -25,9 +59,14 @@ export const CountryList: React.FC<CountryListProps> = ({ listCollection }) => {
       />
       <li className={styles.country_list__title}>
         <div className={styles.country_list__flag}>Flag</div>
-        <div className={styles.country_list__name}>Name</div>
-        <div className={styles.country_list__region}>Region</div>
-        <div className={styles.country_list__population}>Population</div>
+        <button
+          onClick={nameButtonClicked}
+          className={styles.country_list__name}
+        >
+          Name
+        </button>
+        <button className={styles.country_list__region}>Region</button>
+        <button onClick={populationButtonClicked} className={styles.country_list__population}>Population</button>
       </li>
       {countryList.map(({ name, flag, population, region, flags }) => {
         if (isCharsIncluded(search, name.common)) {
